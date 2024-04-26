eefddf3948fd996b4bd85b334b571bc058b1134b
Zakariaaben <141879923+Zakariaaben@users.noreply.github.com>
2024-04-26 22:12:02 +0100

    Update SectionPresentation.jsx

--- src/Components/SectionAccueil/SectionPresentation.jsx
+++ src/Components/SectionAccueil/SectionPresentation.jsx
@@ -8,7 +8,7 @@ const SectionPresentation = () => {
                 <h1>Prenez Soin de <br/>Votre  <span>Santé mentale </span> </h1>
                 <h2>Lorem ipsum dolor sit amet consectetur adipisicing <br/> elit. Placeat harum modi vero  inventore tenetur <br/> accusantium nostrum labore cupiditate.</h2>
             </div>
-            <img src="src\assets\illustrationDoctors.svg" alt="" />
+            <img src="src/assets/illustrationDoctors.svg" alt="" />
     </div>
   )
 }
