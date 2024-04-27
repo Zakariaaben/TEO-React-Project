5823cda26de47a70e8414ab6406d6d31a4033cc5
Zakariaaben <zakaria.benhamiche@gmail.com>
2024-04-27 15:27:26 +0100

    fixed img navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { FaUserDoctor } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 import "./Navbar.css"
 const Navbar = () => {
   return (
     <div className="Navbar">
       <div className="Logo">
-        <img src="/public/Images/couteau.svg" alt=""  style={{height:"30px"}}/>
+        <img src="/Images/couteau.svg" alt=""  style={{height:"30px"}}/>
         <p>MyName</p>
       </div>
       <div className="navbar-links">
