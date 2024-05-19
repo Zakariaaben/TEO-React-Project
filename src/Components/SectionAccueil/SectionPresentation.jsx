fef4b577ce2eadcbc7fe76f88a5a3b956d4c0016
Zakariaaben <zakaria.benhamiche@gmail.com>
2024-05-20 00:00:18 +0100

    updating

--- src/Components/SectionAccueil/SectionPresentation.jsx
+++ src/Components/SectionAccueil/SectionPresentation.jsx
@@ -6,10 +6,10 @@ const SectionPresentation = () => {
     <div className="SectionPresentation">
             <div className="text-content">
                 <h1>Prenez Soin de <br/>Votre  <span>Santé mentale </span> </h1>
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing <br/> elit. Placeat harum modi vero  inventore tenetur <br/> accusantium nostrum labore cupiditate.</h2>
+                <p>Bienvenue sur notre plateforme de prise de rendez-vous avec des psychologues. Nous vous offrons un accès simple et rapide à des professionnels qualifiés, prêts à vous écouter et à vous accompagner dans votre démarche de mieux-être. <br/><br/>Que vous cherchiez de l'aide pour surmonter des défis personnels, gérer le stress ou simplement parler à quelqu'un, nos psychologues sont là pour vous. Prenez rendez-vous en quelques clics et commencez votre chemin vers une vie plus épanouie dès aujourd'hui.</p>
             </div>
             
-            <img src="/Images/illustrationDoctors.svg" alt="" />
+            <img src="/Images/Artboard.png" alt="" />
 
     </div>
   )
